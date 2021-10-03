a6d4a4916d1d285858dda9edbd926b98fb4786c3
Takom <urab535@gmail.com>
2021-10-03 19:00:33 +0300

    feat: project started pack

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: false,
+    trailingComma: 'es5',
+    jsxSingleQuote: true,
+    singleQuote: true,
+    printWidth: 160,
+    tabWidth: 4,
+    endOfLine: 'auto',
+}
---
+++ src/components/Root/Root.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'antd/dist/antd.css'
+import './reset.css'
+import NavigationBar from '../NavigationBar/NavigationBar'
+
+const Root: React.FC = () => {
+    return <NavigationBar />
+}
+
+export default Root
