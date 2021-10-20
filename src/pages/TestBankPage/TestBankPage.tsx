8b542fcf53d0d21a4b2e3270499bc0ee72d524d6
Takom <urab535@gmail.com>
2021-10-20 17:37:13 +0300

    feat: created hooks/spin

--- src/pages/TestBankPage/TestBankPage.tsx
+++ src/pages/TestBankPage/TestBankPage.tsx
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react'
-import { Table } from 'antd'
+import { Row, Col } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadingListTestsAction } from '../../state/tests/testsStateSlice'
+import { Card } from 'antd'
+import { EditOutlined, EllipsisOutlined } from '@ant-design/icons'
+import LoadingSpin from '../../components/spins/LoadingSpin'
+import { loadingListTestsAction, TestBankListInterface } from '../../state/tests/testsStateSlice'
 import { AppStateInterface } from '../../state/_store/createRootReducer'
-const { Column } = Table
+
+const { Meta } = Card
 
 const TestBankPage: React.FC = () => {
     const dispatch = useDispatch()
-    const { testBankList, isTestBankLoading } = useSelector((state: AppStateInterface) => state.tests)
+    const { testBankList, isTestBankLoading, isHaveFirstLoading } = useSelector((state: AppStateInterface) => state.tests)
 
     useEffect(() => {
         dispatch(loadingListTestsAction())
     }, [dispatch])
-
-    useEffect(() => {
-        console.log(testBankList)
-    }, [testBankList])
 
     //const foo = async () => {
     // update
@@ -27,13 +27,24 @@ const TestBankPage: React.FC = () => {
     // }
 
     return (
-        <div>
-            {/*<TestBankAddButton />*/}
-            {/*<Table dataSource={testBankList} loading={isTestBankLoading} key={'1'}>*/}
-            {/*    <Column title={'name'} dataIndex={'name'} />*/}
-            {/*    <Column title={'desctiption'} dataIndex={'desctiption'} />*/}
-            {/*</Table>*/}
-        </div>
+        <>
+            {<LoadingSpin isLoading={isTestBankLoading} top={150} isHaveFirstLoading={isHaveFirstLoading} />}
+            {isHaveFirstLoading && (
+                <Row>
+                    {testBankList.map(({ id, description, name, testsBank }: TestBankListInterface) => (
+                        <Col span={6} key={id} style={{ margin: 16 }}>
+                            <Card
+                                actions={[<EditOutlined key='edit' />, <EllipsisOutlined key='ellipsis' />]}
+                                style={{ width: 300, margin: 16, padding: 15 }}
+                                loading={isTestBankLoading}
+                            >
+                                <Meta title={name} description={description} />
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            )}
+        </>
     )
 }
 
--- src/state/tests/testsStateSlice.ts
+++ src/state/tests/testsStateSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface TestsBankInterface {
+    id: string
     test: string
 }
 
