51355aab985988413fbc274cff0ab84ac75edf34
Takom <urab535@gmail.com>
2021-10-30 13:50:50 +0300

    feat: react pdf installed/carcass created

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, useLocation } from 'react-router-dom'
 import PrivateRoute from '../../utils/privateRoute'
 import { NavigationBar } from '../NavigationBar'
 import { SignInForm } from '../SignInForm'
 import { SignUpForm } from '../SignUpForm'
-import { ProfilePage, TestBankPage, TestsListPage } from '../../pages'
+import { ProfilePage, TestBankPage, TestsListPage, TicketsPage } from '../../pages'
 
 import './reset.css'
 import 'antd/dist/antd.css'
-import { TicketsPage } from '../../pages/TicketsPage'
 
 const Root: React.FC = () => {
+    const location = useLocation()
+    const [displayLocation, setDisplayLocation] = useState(location)
+    const [transitionStage, setTransitionStage] = useState('fadeIn')
+
+    const onAnimationHandler = () => {
+        if (transitionStage === 'fadeOut') {
+            setTransitionStage('fadeIn')
+            setDisplayLocation(location)
+        }
+    }
+
+    useEffect(() => {
+        if (location !== displayLocation) setTransitionStage('fadeOut')
+    }, [location, displayLocation])
+
     return (
-        <Switch>
-            <Route path={'/signup'} component={SignUpForm} />
-            <Route path={'/login'} component={SignInForm} />
-            <NavigationBar>
-                <PrivateRoute redirectPath={'/login'} path={'/tests-bank'} component={TestBankPage} />
-                <PrivateRoute redirectPath={'/login'} path={'/test/:id'} component={TestsListPage} />
-                <PrivateRoute redirectPath={'/login'} path={'/tickets'} component={TicketsPage} />
-                <PrivateRoute redirectPath={'/login'} path={'/profile'} component={ProfilePage} />
-            </NavigationBar>
-        </Switch>
+        <div className={`${transitionStage}`} onAnimationEnd={onAnimationHandler}>
+            <Switch location={displayLocation}>
+                <Route path={'/signup'} component={SignUpForm} />
+                <Route path={'/login'} component={SignInForm} />
+                <NavigationBar>
+                    <PrivateRoute redirectPath={'/login'} path={'/tests-bank'} component={TestBankPage} />
+                    <PrivateRoute redirectPath={'/login'} path={'/test/:id'} component={TestsListPage} />
+                    <PrivateRoute redirectPath={'/login'} path={'/tickets'} component={TicketsPage} />
+                    <PrivateRoute redirectPath={'/login'} path={'/profile'} component={ProfilePage} />
+                </NavigationBar>
+            </Switch>
+        </div>
     )
 }
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './ProfilePage'
 export * from './TestBankPage'
 export * from './TestsListsPage'
+export * from './TicketsPage'
